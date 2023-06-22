ec7bc7f16549f6a1f26d5ec8b74010110db8c8c4
fendishatech <liyukibir@gmail.com>
2023-06-22 09:01:03 -0700

    low quantity items

--- resources/views/dashboard/partials/calendar.blade.php
+++ resources/views/dashboard/partials/calendar.blade.php
@@ -11,14 +11,36 @@
 </div>
 
 <div class="mt-6 flex flex-col rounded-lg shadow-lg">
-    <div class="w-full pt-6 flex items-center flex-col rounded-t-lg bg-green-300">
-        <div class="text-2xl font-bold mb-4">{{ $currentDay }}</div>
-        <div id="real-time" class="text-gray-500 text-lg mb-4">
-            {{ $currentTime }}
-        </div>
+    <div class="w-full pt-6 flex items-center flex-col rounded-t-lg bg-red-500 text-white">
+        <div class="text-2xl font-bold mb-4">Low Quantity Items</div>
     </div>
     <div class="text-gray-500 text-lg py-6">
-        <h1 class="text-center font-semibold text-2xl">{{ $currentDate }}</h1>
+        <div class="overflow-x-auto border-b border-gray-200 shadow">
+            <table class="divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th class="px-6 py-2 text-xs text-gray-500">Item Name</th>
+                        <th class="px-6 py-2 text-xs text-gray-500">Quantity</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-300">
+                    @foreach ($items as $item)
+                        <tr class="whitespace-nowrap">
+                            <td class="px-6 py-4">
+                                <div class="text-sm text-gray-900">
+                                    {{ $item->item_name }}
+                                </div>
+                            </td>
+                            <td class="px-6 py-4">
+                                <div class="text-sm text-gray-500">
+                                    {{ $item->partnumber }}
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 
