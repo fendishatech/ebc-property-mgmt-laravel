f8e229aec4b585f7b8a4b303c1162b496e3c2b21
fendishatech <liyukibir@gmail.com>
2023-06-27 07:26:26 -0700

    edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -56,9 +56,9 @@
                     <div class="relative">
                         <select
                             class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-                            id="item_id" name="user_role">
-                            <option value="user">User</option>
-                            <option value="admin">Admin</option>
+                            id="user_id" name="user_role">
+                            <option value="user" {{ $user->user_role == 'user' ? 'selected' : '' }}>User</option>
+                            <option value="admin" {{ $user->user_role == 'admin' ? 'selected' : '' }}>Admin</option>
                         </select>
                         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                             <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
